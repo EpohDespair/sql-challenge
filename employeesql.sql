85ab5f0f47008850951b21841be16d553ad33faa
Hope Diebold <epohdespair@gmail.com>
2021-11-12 17:08:36 -0500

    added ERD outline and SQL file

---
+++ employeesql.sql
@@ -0,0 +1,16 @@
+--Employee SQL homework
+--create tables for all 6 files
+
+-- create table employees(
+-- 	emp_no integer not null,
+-- 	emp_title_id varchar(250) not null,
+-- 	birth_date date not null,
+-- 	first_name varchar(250) not null,
+-- 	last_name varchar(250) not null,
+-- 	sex varchar(1) not null,
+-- 	hire_date date not null,
+-- 	primary key(emp_no)
+-- );
+
+--check table headers, import data, check table again
+select * from employees;
