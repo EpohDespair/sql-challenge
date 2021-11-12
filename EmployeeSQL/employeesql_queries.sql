4159074d3a5f66720052cb58b175115a161c11a0
Hope Diebold <epohdespair@gmail.com>
2021-11-12 18:56:50 -0500

    updated queries file

---
+++ EmployeeSQL/employeesql_queries.sql
@@ -0,0 +1,37 @@
+-- -- Data Analysis
+
+-- -- list employee number, last name, first name, sex and salary
+-- select
+-- employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
+-- from employees
+-- join salaries 
+-- on employees.emp_no = salaries.emp_no
+
+-- -- list first name, last name, hire date for employees who were hired in 1986
+-- select
+-- employees.last_name, employees.first_name, employees.hire_date
+-- from employees
+-- where employees.hire_date >'1986-1-1' AND employees.hire_date< '1987-1-1'
+
+-- -- list the manager of each dept: dept number, dept name, mgr's employee number, last name, first name
+-- select
+-- departments.dept_no, departments.dept_name, dept_manager.emp_no, employees.last_name, employees.first_name
+-- from dept_manager
+-- join employees
+-- on dept_manager.emp_no = employees.emp_no
+-- join departments
+-- on dept_manager.dept_no = departments.dept_no
+
+-- -- list the dept of each employee, employee number, last name, first name, dept name
+-- select
+-- dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
+-- from dept_emp
+-- join employees
+-- on dept_emp.emp_no = employees.emp_no
+-- join departments
+-- on dept_emp.dept_no = departments.dept_no
+
+-- -- list first name, last name, sex where first name = Hercules and last names start with B
+-- -- list all employees in Sales: employee number, last name, first name, dept name
+-- -- list all in sales and Dev: employee number, last name, first name, dept name
+-- -- descending order, list frequency count of employee last names(group by)
